b08e77aa46f1325807a44424aa98ca11075cc942
Jackie Yeh <jackie@tellustek.com>
2015-10-12 15:44:44 +0800

    fix webhook.php

--- webhook.php
+++ webhook.php
@@ -7,7 +7,7 @@ $BRANCH             = "master";
 if ( $_POST['payload'] ) {
   // Only respond to POST requests from Github
   // If there is already a repo, just run a git pull to grab the latest changes
-  shell_exec("cd {$LOCAL_REPO_ROOT} && git pull");
+  shell_exec("cd {$LOCAL_REPO_ROOT} && git pull origin master");
   var_dump($_POST['payload']);
   die("done " . mktime());
 }
